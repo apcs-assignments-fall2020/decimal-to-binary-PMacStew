6b8d6cd8a713cf73fb31949e7dffae12f77b7196
PMacStew <23pmcclure@athenian.org>
2020-09-15 21:10:37 +0000

    finish

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,23 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int computer = Integer.parseInt(binary);
+        int num = 0;
+        int count = 1;
+        while (computer != 0) {
+            num += (computer % 10) * count;
+            computer /= 10;
+            count += count;
+        }
+        return num;
+
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a binary number: ");
+        String binary = scan.next();
+        System.out.println("The value of your binary number is " + binaryToDecimal(binary));
     }
 }
